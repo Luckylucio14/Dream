95eb94735aba98fa62eec2d4d36f53152b0e0cfe
vincent le callonnec <lecallonnec.v@gmail.com>
2022-11-23 15:52:40 +0100

    fix index ctrl

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,7 +1,11 @@
 class StarsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
+    def index
+      @stars = Star.all
+    end
+
     def show
       @star = Star.find(params[:id])
     end
-end
+  end
