e05bf790ece56784b4bf0b4267b1954fd2d2ad68
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-11-23 11:16:11 +0100

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Star.destroy_all
+
+alice        = User.create!(first_name: alice, last_name: pont )
+
+alice_star_1 = Star.create!(,)
+
+Booking.create!(
+  star:        star1,
+  user:        User2,
+  starting_on: "2022-11-22",
+  ending_on:   "2022-11-24",
+  status:      "accepted"
+)
+
+Booking.create!(
+  star:        star2,
+  user:        User1,
+  starting_on: "2022-11-22",
+  ending_on:   "2022-11-25",
+  status:      "pending"
+)
+
+Booking.create!(
+  star:        star3,
+  user:        User3,
+  starting_on: "2022-11-24",
+  ending_on:   "2022-11-25",
+  status:      "decline"
+)
