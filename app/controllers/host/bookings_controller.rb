1e0461da365e62498033f72973ea0a5ab4f469cd
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-11-25 12:37:59 +0100

    button accepted or declined rajouté

---
+++ app/controllers/host/bookings_controller.rb
@@ -0,0 +1,15 @@
+class Host::BookingsController < ApplicationController
+  # skip_before_action :authenticate_user!, only: %i[index accept decline]
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "accepted")
+    redirect_to dashboard_path
+    
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "declined")
+    redirect_to dashboard_path
+  end
+end
